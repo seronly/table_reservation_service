fbe2d5e9c04ba7f121270e80864b0d5ae9cd4a25
SerOnly <seronly@mail.ru>
2025-04-09 12:06:22 +0700

    refactor: change routers

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from . import auth, users
+from . import tables, reservations
 
 api_router = APIRouter()
-api_router.include_router(auth.router)
-api_router.include_router(users.router)
+api_router.include_router(tables.router)
+api_router.include_router(reservations.router)
