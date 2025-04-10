d606af9e97396e57140a8609b05fc89131ef6d38
SerOnly <seronly@mail.ru>
2025-04-10 15:18:18 +0700

    feat: add tables api

--- app/api/routers/tables.py
+++ app/api/routers/tables.py
@@ -1,7 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import create_session
+from app.schemas.table import TableCreate, Table
+from app.services import table_services
 
 router = APIRouter(prefix="/tables", tags=["tables"])
 
 @router.get("/")
-def get_table():
-    return {"message": "No tables right now"}
+async def get_table(session: AsyncSession = Depends(create_session)) -> list[Table] | dict:
+    tables = await table_services.get_tables(session)
+    if not tables:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Tables not found"
+        )
+    return tables
+
+@router.post("/")
+async def create_table(table: TableCreate, session: AsyncSession = Depends(create_session)) -> TableCreate:
+    db_table = await table_services.create_table(table, session)
+    if not db_table:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Table not created"
+        )
+    return db_table
+
+@router.delete("/{id}")
+async def delete_table(id: int, session: AsyncSession = Depends(create_session)) -> JSONResponse:
+    table_deleted = await table_services.delete_table_by_id(id, session)
+    if not table_deleted:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Table with id {id} not found"
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"message": f"Table with id {id} deleted"}
+    )
---
+++ app/services/table_services.py
@@ -0,0 +1,41 @@
+import logging
+from sqlalchemy import select, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.table import Table
+from app.schemas.table import TableCreate
+
+
+logger = logging.getLogger(__name__)
+
+async def create_table(table: TableCreate, session: AsyncSession) -> Table | None:
+    table_db = Table(**table.model_dump())
+    try:
+        session.add(table_db)
+        await session.commit()
+        await session.refresh(table_db)
+        return table_db
+    except Exception as e:
+        logger.error(f"Error creating table: {e}")
+        await session.rollback()
+        return None
+
+async def get_tables(session: AsyncSession) -> list[Table]:
+    query = select(Table)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+async def get_table_by_id(id: int, session: AsyncSession) -> Table:
+    query = select(Table).where(Table.id == id)
+    result = await session.execute(query)
+    return result.first()
+
+async def delete_table_by_id(id: int, session: AsyncSession) -> bool:
+    table = await get_table_by_id(id, session)
+    if not table:
+        return False
+
+    query = delete(Table).where(Table.id == id)
+    await session.execute(query)
+    await session.commit()
+    return True
